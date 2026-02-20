89987f52a4850e3e2e060a7797bd2ee4c82667ca
Skip <skip@usecurling.com>
2026-02-20 17:24:04 +0000

    Fix toast notification overlap with cart button

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:right-0 sm:flex-col md:max-w-[420px]',
+      'fixed top-16 sm:top-20 z-[100] flex max-h-[calc(100vh-4rem)] sm:max-h-[calc(100vh-5rem)] w-full flex-col-reverse p-4 sm:right-0 sm:flex-col md:max-w-[420px]',
       className,
     )}
     {...props}
